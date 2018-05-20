bc0120befc7759239a70ed2825993176b49d7e84
Qiu <qiu.gits@gmail.com>
2018-05-21 00:48:19 +0900

    Generate Food model. Add food feed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_11_084642) do
+ActiveRecord::Schema.define(version: 2018_05_20_135102) do
+
+  create_table "foods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "foodname"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_foods_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,4 +27,5 @@ ActiveRecord::Schema.define(version: 2018_05_11_084642) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
 end
