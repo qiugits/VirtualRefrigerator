569d10261c53bcffc71df8cfd48213d62097798f
qiugits <qiu.gits@gmail.com>
2018-06-04 13:30:18 +0900

    Show sum of nutritions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,30 @@
 User.create!(name: "Ikuko")
-
 user = User.first
-# https://github.com/stympy/faker/blob/master/doc/food.md
-50.times do
-  food = Faker::Food.ingredient
-  kind = FoodKind.create(
-    kindname: food,
-    nutrition_a: 20
-  )
-  user.foods.create!(
-    foodname: food,
-    amount: 1,
-    kind_id: kind.id
-  )
-end
 
 
 require 'csv'
 CSV.foreach(Rails.root + 'db/nutr_data_utf8.csv') do |row|
-	FoodKind.create!(:kindname => row[0], :protein => row[1], :lipd => row[2], :carbon => row[3],:vitamin => row[4], :minerals => row[5])
+	FoodKind.create!(
+    :kindname => row[0], 
+    :protein => row[1],
+    :lipd => row[2], 
+    :carbon => row[3],
+    :vitamin => row[4],
+    :minerals => row[5]
+  )
 end
 
+50.times do
+  kind = FoodKind.offset(rand(FoodKind.count)).first
+  amount = rand(5)
+  user.foods.create!(
+    foodname: kind.kindname,
+    kind_id: kind.id,
+    amount: amount,
+    protein: kind.protein * amount,
+    lipd: kind.lipd * amount,
+    carbon: kind.carbon * amount,
+    vitamin: kind.vitamin * amount,
+    minerals: kind.minerals * amount,
+  )
+end
