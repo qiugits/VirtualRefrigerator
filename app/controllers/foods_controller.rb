ea93f922e436f6c208de609c88fd89cc02eb0125
qiugits <qiu.gits@gmail.com>
2018-06-11 21:12:25 +0900

    Separate update_nutri as model private func

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -35,20 +35,8 @@ class FoodsController < ApplicationController
 
   def update
     @food = Food.find(params[:id])
-    kind = FoodKind.find(@food.kind_id)
     if @food.update_attributes(food_params)
-      if @food.amount <= 0
-        @food = Food.find(params[:id]).destroy
-        redirect_to user_foods_path(@food.user_id)
-      else
-        @food.protein = kind.protein * @food.amount
-        @food.lipd = kind.lipd * @food.amount
-        @food.carbon = kind.carbon * @food.amount
-        @food.vitamin = kind.vitamin * @food.amount
-        @food.minerals = kind.minerals * @food.amount
-        @food.save
-        redirect_to user_foods_path(@food.user_id)
-      end
+      redirect_to user_foods_path(@food.user_id)
     else
       redirect_to root_url
     end
--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,20 @@ class Food < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
+  before_save :update_nutri
+
+  private
+  def update_nutri
+    if self.amount <= 0
+      #self = Food.find(params[:id]).destroy
+      self.destroy
+    else
+      kind = FoodKind.find(self.kind_id)
+      self.protein = kind.protein * self.amount
+      self.lipd = kind.lipd * self.amount
+      self.carbon = kind.carbon * self.amount
+      self.vitamin = kind.vitamin * self.amount
+      self.minerals = kind.minerals * self.amount
+    end
+  end
 end
