8a571142750c7a878c9ed9b42e002f38aa0417c8
qiugits <qiu.gits@gmail.com>
2018-06-16 21:49:00 +0900

    Improve apperance

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
